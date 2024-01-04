f3e8a3436966f46b4a538e62fa202a2fe16b3bb5
MatusKovac <matus.kovac@kosickaakademia.sk>
2024-01-04 18:15:13 +0100

    change styling for post card

--- components/home/HomePage.js
+++ components/home/HomePage.js
@@ -75,14 +75,28 @@
 // };
 
 import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import axios, {options} from 'axios';
 import {BASE_URL} from '../../utils/Constants';
 import {err} from 'react-native-svg/lib/typescript/xml';
 import {getItem} from '../../utils/Storage';
+import {useTheme} from '../../utils/ThemeContext';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faComment, faHeart} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+
+library.add(faHeart, faComment);
 
 const PAGE_SIZE = 3;
 const HomePage = ({navigation}) => {
+  const {theme, toggleTheme} = useTheme();
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
 
@@ -97,7 +111,7 @@ const HomePage = ({navigation}) => {
           },
           {
             headers: {
-              Authorization: await getItem("Authorization"),
+              Authorization: await getItem('Authorization'),
               USER_ID: await getItem('USER_ID'),
               // Add other headers if needed
             },
@@ -118,24 +132,97 @@ const HomePage = ({navigation}) => {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
     },
-    image: {
-      width: 100,
-      height: 100,
+    card: {
+      backgroundColor: theme === 'dark' ? '#2a2a2a' : '#ffffff',
+      marginVertical: 15,
+    },
+    cardImage: {
+      width: '100%',
+      height: 300,
+    },
+    cardHeader: {
+      marginTop: 10,
+      padding: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      gap: 3,
+    },
+    cardInteraction: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      padding: 10,
+      borderWidth: 0.25,
+      borderColor: theme === 'dark' ? '#ffffff' : '#000000',
+    },
+    cardTitle: {
+      color: theme === 'dark' ? '#ffffff' : '#000000',
+    },
+    cardAvatar: {
+      marginRight: 16,
+      width: 40,
+      height: 40,
+    },
+    cardContent: {
+      padding: 10,
+      borderWidth: 0.25,
+      borderColor: theme === 'dark' ? '#ffffff' : '#000000',
+      color: theme === 'dark' ? '#ffffff' : '#000000',
     },
   });
 
   const renderItem = ({item}) => {
     return (
-      <View style={styles.container}>
-        <Text>{item.description}</Text>
+      // <View style={styles.container}>
+      //   <Text>{item.username}</Text>
+      //   <Image
+      //     source={{uri: `data:image/png;base64,${item.image}`}}
+      //     style={styles.image}
+      //   />
+      //   <Text>{item.description}</Text>
+      //   <View style={styles.interaction}>
+      //     <Text>{item.likes}</Text>
+      //     <Text>ahoj</Text>
+      //   </View>
+      // </View>
+
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <TouchableOpacity>
+            <Image
+              source={require('../../assets/background/profile.png')}
+              style={styles.cardAvatar}
+            />
+          </TouchableOpacity>
+          <Text style={styles.cardTitle}>{item.username}</Text>
+        </View>
         <Image
           source={{uri: `data:image/png;base64,${item.image}`}}
-          style={styles.image}
+          style={styles.cardImage}
         />
-        <Text>{item.username}</Text>
+        <View style={styles.cardInteraction}>
+          <FontAwesomeIcon
+            icon={'heart'}
+            color={theme === 'dark' ? '#ffffff' : '#2a2a2a'}
+            size={30}
+          />
+          <FontAwesomeIcon
+            icon={'comment'}
+            color={theme === 'dark' ? '#ffffff' : '#2a2a2a'}
+            size={30}
+          />
+        </View>
+        <View style={styles.cardContent}>
+          <Text style={{color: theme === 'dark' ? '#ffffff' : '#000000'}}>
+            {item.description}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+            aliquid animi commodi earum eligendi fuga fugit minus molestiae
+            necessitatibus quam qui quibusdam quidem quis saepe unde!
+            Exercitationem nam sint temporibus.
+          </Text>
+        </View>
       </View>
     );
   };
